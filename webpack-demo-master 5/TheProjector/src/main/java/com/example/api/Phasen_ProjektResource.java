30495c82bc4dac6510a4c73edbeb77b2a4efd2ed
Annika Cagitz <annika.cagitzz@gmail.com>
2023-03-21 15:04:21 +0100

    Update Phasen_ProjektResource.java

--- webpack-demo-master 5/TheProjector/src/main/java/com/example/api/Phasen_ProjektResource.java
+++ webpack-demo-master 5/TheProjector/src/main/java/com/example/api/Phasen_ProjektResource.java
@@ -22,7 +22,7 @@ public class Phasen_ProjektResource {
     }
 
     @GET
-    @Path("/all")
+    @Path("/getAll")
     public Response getAll() {
         var all = this.phasen_projektRepo.listAll();
         return Response.ok(all).build();
