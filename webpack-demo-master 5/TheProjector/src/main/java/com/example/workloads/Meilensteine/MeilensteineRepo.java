4158ab25dda08aa128c385b3e85ae790b5f3e795
David Ursprung <59611676+DavidUrsprung@users.noreply.github.com>
2023-03-23 13:08:11 +0100

    Update MeilensteineRepo.java

--- webpack-demo-master 5/TheProjector/src/main/java/com/example/workloads/Meilensteine/MeilensteineRepo.java
+++ webpack-demo-master 5/TheProjector/src/main/java/com/example/workloads/Meilensteine/MeilensteineRepo.java
@@ -3,9 +3,16 @@ package com.example.workloads.Meilensteine;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.Query;
+import java.util.List;
 
 @ApplicationScoped
 public class MeilensteineRepo implements PanacheRepository<Meilensteine> {
+
+    public List<Meilensteine> getAllSorted() {
+        Query query = this.getEntityManager().createQuery("select m from Meilensteine m order by m.end_datum asc");
+        return query.getResultList();
+    }
 
     public void update(Meilensteine meilensteine) {
         this.getEntityManager().merge(meilensteine);
